f07a4e53536c3f3f22e0202405284521c361cb83
mitis-ezdi <miti.s@ezdi.us>
2016-09-30 16:34:46 +0530

    Update App.java

--- src/main/java/com/MavenSample/FirstMaven/App.java
+++ src/main/java/com/MavenSample/FirstMaven/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hii Change from Github to check the synchronization..part 2..." );
+        System.out.println( "Hii Change from Github to check the synchronization..part 3..." );
     }
 }
